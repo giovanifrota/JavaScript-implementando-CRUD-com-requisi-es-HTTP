da6b78b46a8ef8500a15bcf2b7b37901967bf1ca
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2026-07-22 22:11:29 -0300

    Adicionando dados ao POST

--- js/api.js
+++ js/api.js
@@ -8,6 +8,22 @@ const api = {
       throw error;
     }
   },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await fetch("http://localhost:3000/pensamentos", {
+        method: "POST",
+        headers: {
+          "CONTENT-TYPE": "application/body",
+        },
+        body: JSON.stringify(pensamento),
+      });
+      return response.json();
+    } catch {
+      alert("ERRO AO BUSCAR PENSAMENTOS! ");
+      throw error;
+    }
+  },
 };
 
 export default api;
